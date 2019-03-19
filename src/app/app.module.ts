eb6f77d17a1862b32536c0057258e5364072f0bd
vladek1934 <vlad.mun1999@gmail.com>
2019-03-19 16:41:59 +0600

    alpha 1.3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ItemComponent } from './item/item.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AboutComponent } from './about/about.component';
+import { ItemFullInfoComponent } from './item-full-info/item-full-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ItemComponent
+    ItemComponent,
+    ProfileComponent,
+    LoginComponent,
+    RegisterComponent,
+    AboutComponent,
+    ItemFullInfoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/item-full-info/item-full-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-full-info',
+  templateUrl: './item-full-info.component.html',
+  styleUrls: ['./item-full-info.component.css']
+})
+export class ItemFullInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
