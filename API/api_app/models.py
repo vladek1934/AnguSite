79508a68b597913334d79a63370f0ad8ff5846c7
vladek1934 <vlad.mun1999@gmail.com>
2019-05-13 22:00:15 +0600

    models

--- API/api_app/models.py
+++ API/api_app/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class OrderManager(models.Manager):
+    def for_user(self, user):
+        return self.filter(created_by=user)
+
+
+class Order(models.Model):
+    name = models.CharField(max_length=200)
+    date = models.DateTimeField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+
+    objects = OrderManager()
+
+    class Meta:
+        verbose_name = 'Order'
+        verbose_name_plural = 'Orders'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+    price = models.IntegerField()
+    count = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+
+
+class Comment(models.Model):
+    text = models.CharField(max_length=350)
+    rating = models.IntegerField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, default=None, required=True)
