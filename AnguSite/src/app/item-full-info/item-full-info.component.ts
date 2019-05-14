8aad9a61c9ab8699b67662c7894f07872389339f
Dmitriy Tuchashvili <pechorin211@gmail.com>
2019-05-15 00:36:12 +0600

    zooming works

--- AnguSite/src/app/item-full-info/item-full-info.component.ts
+++ AnguSite/src/app/item-full-info/item-full-info.component.ts
@@ -2,8 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Product} from '../shared/models/models';
 import {ProviderService} from '../shared/services/provider.service';
-import {ɵDOMTestComponentRenderer} from "@angular/platform-browser-dynamic/testing";
-import {decreaseElementDepthCount} from "@angular/core/src/render3/state";
+import {ɵDOMTestComponentRenderer} from '@angular/platform-browser-dynamic/testing';
+import {decreaseElementDepthCount} from '@angular/core/src/render3/state';
 
 @Component({
   selector: 'app-item-full-info',
@@ -22,37 +22,50 @@ export class ItemFullInfoComponent implements OnInit {
 
   constructor(private route: ActivatedRoute, private provider: ProviderService) { }
 
-  mouseEnter(imageId, zoomId) {
+  mouseEnter(imageId, zoomId, areaId) {
     const image = document.getElementById(imageId);
     document.getElementById(zoomId).style.left = image.offsetLeft + image.offsetWidth + 'px';
     document.getElementById(zoomId).style.top = image.offsetTop + 'px';
     document.getElementById(zoomId).style.width = image.offsetWidth + 'px';
     document.getElementById(zoomId).style.height = image.offsetHeight + 'px';
     document.getElementById(zoomId).style.display = 'block';
+
+    document.getElementById(areaId).style.display = 'block';
   }
-  mouseLeave(zoomId) {
+  mouseLeave(zoomId, areaId) {
     document.getElementById(zoomId).style.display = 'none';
+
+    document.getElementById(areaId).style.display = 'none';
   }
 
-  getMousePos(event, imageId, zoomImgId) {
+  getMousePos(event, imageId, zoomImgId, areaId) {
+    const zoomLevel = 2;
+
     const image = document.getElementById(imageId);
     let x = event.x - image.offsetLeft + document.documentElement.scrollLeft;
     let y = event.y - image.offsetTop + document.documentElement.scrollTop;
 
-    if (x < image.offsetWidth / 8) {
-      x = image.offsetWidth / 8;
+    if (x < image.offsetWidth / zoomLevel / 2) {
+      x = image.offsetWidth / zoomLevel / 2;
     }
-    if (x > image.offsetWidth * 7 / 8) {
-      x = image.offsetWidth * 7 / 8;
+    if (x > image.offsetWidth * (zoomLevel * 2 - 1) / zoomLevel / 2) {
+      x = image.offsetWidth * (zoomLevel * 2 - 1) / zoomLevel / 2;
     }
-    if (y < image.offsetHeight / 8) {
-      y = image.offsetHeight / 8;
+    if (y < image.offsetHeight / zoomLevel / 2) {
+      y = image.offsetHeight / zoomLevel / 2;
     }
-    if (y > image.offsetHeight * 7 / 8) {
-      y = image.offsetHeight * 7 / 8;
+    if (y > image.offsetHeight * (zoomLevel * 2 - 1) / zoomLevel / 2) {
+      y = image.offsetHeight * (zoomLevel * 2 - 1) / zoomLevel / 2;
     }
-    document.getElementById(zoomImgId).style.marginLeft = -x * 4 + image.offsetWidth * 2 + 'px';
-    document.getElementById(zoomImgId).style.marginTop = -y * 4 + image.offsetHeight * 2 + 'px';
+    document.getElementById(zoomImgId).style.marginLeft = -x * zoomLevel + image.offsetWidth * zoomLevel / 2 + 'px';
+    document.getElementById(zoomImgId).style.marginTop = -y * zoomLevel + image.offsetHeight * zoomLevel / 2 + 'px';
+    document.getElementById(zoomImgId).style.transform = 'scale(' + zoomLevel + ')';
+
+    document.getElementById(areaId).style.left = (x + image.offsetLeft - image.offsetWidth / zoomLevel / 2) + 'px';
+    document.getElementById(areaId).style.top = (y + image.offsetTop - image.offsetHeight / zoomLevel / 2) + 'px';
+    document.getElementById(areaId).style.width = (image.offsetWidth / zoomLevel) + 'px';
+    document.getElementById(areaId).style.height = (image.offsetHeight / zoomLevel) + 'px';
+
   }
 
   ngOnInit() {
